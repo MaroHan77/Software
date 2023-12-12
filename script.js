54f7cd7edd6e9624d805ae197426cd805ea3a359
Maro <callmeyami7@gmail.com>
2023-12-12 15:51:40 +0900

    css 수정
    - css 헤더와 푸터 색상 약간 하늘색으로 수정
    - script.js 파일 주석 추가

--- script.js
+++ script.js
@@ -10,16 +10,22 @@ function displaySearchResults() {
     // 검색어에 따라 다른 종류의 iframe 주소 설정
     if (searchTerm === '넷플릭스' || searchTerm === '공포' || searchTerm === '스위트홈') {
     iframeSrc = 'https://www.youtube.com/embed/VMZBhtnCk4I';
-}
+    }
     else if (searchTerm === '게임' || searchTerm === '롤') {
     iframeSrc = 'https://www.youtube.com/embed/C3GouGa0noM';
-}
+    }
     else if (searchTerm === '뉴진스' || searchTerm === '아이돌' || searchTerm === '뮤비') {
     iframeSrc = 'https://www.youtube.com/embed/Km71Rr9K-Bw';
-}
+    }
+    // 몇개 경우의 수 더 추가하기
 
 
-    // 검색 결과를 표시할 공간에 iframe 추가 또는 '검색결과가 존재하지 않습니다!' 표시
+    // 원래는 특정 알고리즘에 의한 추천이 필요하고 동영상을 띄워줘야 함
+    // 알고리즘을은 특정 단어와 연관된 영상을 띄워주는 것으로 대체
+    // 동영상을 저장하고 띄워놓기 어려워 부득이하게 유튜브 영상을 iframe에 띄우는 방식 사용
+
+
+    // 검색 결과를 표시할 공간에 iframe 추가 또는 '해당 키워드로는 컨텐츠 추천이 어려워요 ㅜ.ㅠ' 표시
     if (iframeSrc) {
     var iframe = document.createElement("iframe");
     iframe.width = '640';
@@ -27,7 +33,8 @@ function displaySearchResults() {
     iframe.src = iframeSrc;
     searchResultsSection.innerHTML = '<h3>추천된 컨텐츠!</h3>';
     searchResultsSection.appendChild(iframe);
-} else {
-    searchResultsSection.innerHTML = '<h3>해당 키워드로는 컨텐츠 추천이 어려워요 ㅜ.ㅠ</h3>';
-}
+    }
+    else {
+    searchResultsSection.innerHTML = '<h3> " ' + searchTerm + ' " 키워드로는 컨텐츠 추천이 어려워요 ㅜ.ㅠ</h3>';
+    }
 }
