31e363f76481084c7fcaacd4bd7bd91fdfbd952e
Maro <callmeyami7@gmail.com>
2023-12-12 16:46:39 +0900

    스크립트 수정
    - tags 추가하여 한개의 키워드에 여러개의 영상 주소를 매칭시킬수 있도록 수정
    - 태그를 검색했을 때 해당 태그에 연결되어있는 모든 영상 주소를 나타낼수 있도록 배열을 사용하는 방식으로 수정

--- script.js
+++ script.js
@@ -6,35 +6,34 @@ function displaySearchResults() {
     var searchResultsSection = document.getElementById("search-results");
 
     // 검색어에 따라 다른 종류의 iframe 주소 설정
-    var iframeSrc = '';
-    // 검색어에 따라 다른 종류의 iframe 주소 설정
-    if (searchTerm === '넷플릭스' || searchTerm === '공포' || searchTerm === '스위트홈') {
-    iframeSrc = 'https://www.youtube.com/embed/VMZBhtnCk4I';
-    }
-    else if (searchTerm === '게임' || searchTerm === '롤') {
-    iframeSrc = 'https://www.youtube.com/embed/C3GouGa0noM';
-    }
-    else if (searchTerm === '뉴진스' || searchTerm === '아이돌' || searchTerm === '뮤비') {
-    iframeSrc = 'https://www.youtube.com/embed/Km71Rr9K-Bw';
-    }
-    // 몇개 경우의 수 더 추가하기
+    var tags = {
+        '넷플릭스': ['https://www.youtube.com/embed/VMZBhtnCk4I', 'https://www.youtube.com/embed/2Aa_W61eqKY', 'https://www.youtube.com/embed/B_bPHHLkLu8'],
+        '공포': ['https://www.youtube.com/embed/VMZBhtnCk4I', 'https://www.youtube.com/embed/gk9urSdhbzg'],
+        '스위트홈': ['https://www.youtube.com/embed/VMZBhtnCk4I'],
+        '게임': ['https://www.youtube.com/embed/C3GouGa0noM', 'https://www.youtube.com/embed/CjaJVaTzt-4'],
+        '롤': ['https://www.youtube.com/embed/C3GouGa0noM', 'https://www.youtube.com/embed/CjaJVaTzt-4'],
+        '뉴진스': ['https://www.youtube.com/embed/Km71Rr9K-Bw', 'https://www.youtube.com/embed/C3GouGa0noM', 'https://www.youtube.com/embed/ArmDp-zijuc'],
+        '아이돌': ['https://www.youtube.com/embed/Km71Rr9K-Bw', 'https://www.youtube.com/embed/ArmDp-zijuc'],
+        '뮤비': ['https://www.youtube.com/embed/Km71Rr9K-Bw', 'https://www.youtube.com/embed/gdZLi9oWNZg'],
+        '행복': ['https://www.youtube.com/embed/B_bPHHLkLu8'],
+        // 다른 태그들도 필요에 따라 추가
+    };
 
+    // gpt api를 활용하여 영상에 적절한 태그를 추가하는 기능을 개발하고 싶었으나..
 
-    // 원래는 특정 알고리즘에 의한 추천이 필요하고 동영상을 띄워줘야 함
-    // 알고리즘을은 특정 단어와 연관된 영상을 띄워주는 것으로 대체
-    // 동영상을 저장하고 띄워놓기 어려워 부득이하게 유튜브 영상을 iframe에 띄우는 방식 사용
-
+    var iframeSrcs = tags[searchTerm];
 
     // 검색 결과를 표시할 공간에 iframe 추가 또는 '해당 키워드로는 컨텐츠 추천이 어려워요 ㅜ.ㅠ' 표시
-    if (iframeSrc) {
-    var iframe = document.createElement("iframe");
-    iframe.width = '640';
-    iframe.height = '360';
-    iframe.src = iframeSrc;
-    searchResultsSection.innerHTML = '<h3>추천된 컨텐츠!</h3>';
-    searchResultsSection.appendChild(iframe);
-    }
-    else {
-    searchResultsSection.innerHTML = '<h3> " ' + searchTerm + ' " 키워드로는 컨텐츠 추천이 어려워요 ㅜ.ㅠ</h3>';
+    if (iframeSrcs && iframeSrcs.length > 0) {
+        searchResultsSection.innerHTML = '<h3>" ' + searchTerm + ' " 키워드로 추천된 컨텐츠!</h3>';
+        iframeSrcs.forEach(function (src) {
+            var iframe = document.createElement("iframe");
+            iframe.width = '640';
+            iframe.height = '360';
+            iframe.src = src;
+            searchResultsSection.appendChild(iframe);
+        });
+    } else {
+        searchResultsSection.innerHTML = '<h3> " ' + searchTerm + ' " 키워드로는 컨텐츠 추천이 어려워요 ㅜ.ㅠ</h3>';
     }
 }
